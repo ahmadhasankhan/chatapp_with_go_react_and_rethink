8a2f0a91cb1c0e2a44749162cd1e22822f047de5
Ahmad Hassan <er.ahmad.hassan@gmail.com>
2016-02-15 03:16:51 +0530

    Print the channel list

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+let channels = [
+{name: 'Hardware Support'},
+{name: 'Software Support'},
+{name: 'IT Support'},
+{name: 'Health Support'},
+]
 var Channel = React.createClass({
   
   onClick(){
@@ -6,13 +12,31 @@ var Channel = React.createClass({
 
   render: function() {
     return (
-      <li onClick={this.onClick.bind(this)}>
+      <li onClick={this.onClick}>
         {this.props.name} 
       </li>
     );
   }
 });
+
+var ChannelList= React.createClass({
+  render: function() {
+    return (
+      <ul>
+        {this.props.channels.map(channel => {
+        		return(
+        			<Channel name = {channel.name} />
+
+        		)
+        	}
+        )}
+      </ul>
+    );
+  }
+});
+
+
 ReactDOM.render(
-  <Channel name =  'Hardware Support'/>,
+  <ChannelList channels = {channels} />,
   document.getElementById('app')
 );
